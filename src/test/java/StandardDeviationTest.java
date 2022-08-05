4020e6770a12708c39f81d5e2a321c1cd9624a5c
PrathikaSuvarna <prathikasuvarna96gmail.com>
2022-08-04 20:30:56 -0400

    Problem2 latex and pdf file added.

--- src/test/java/StandardDeviationTest.java
+++ src/test/java/StandardDeviationTest.java
@@ -1,8 +1,6 @@
 import org.junit.Assert;
 import org.junit.Test;
-
 import java.text.DecimalFormat;
-//import junit.framework.Assert;
 
 public class StandardDeviationTest {
     DecimalFormat df = new DecimalFormat("#.#############");
@@ -12,6 +10,10 @@ public class StandardDeviationTest {
 
     }
 
+    /**
+     * Unit test for Function Requirement: R1
+     * @throws Exception
+     */
     @Test
     public void testZero() throws Exception {
         arrSinglenumber[0]=0;
@@ -19,7 +21,10 @@ public class StandardDeviationTest {
         double expectres = 0;
         Assert.assertTrue(res==expectres);
     }
-
+    /**
+     * Unit test for Function Requirement: R2
+     * @throws Exception
+     */
     @Test
     public void testSingleNumber() throws Exception {
         arrSinglenumber[0]=5;
@@ -27,7 +32,10 @@ public class StandardDeviationTest {
         double expectres = 0;
         Assert.assertTrue(res==expectres);
     }
-
+    /**
+     * Unit test for Function Requirement: R3
+     * @throws Exception
+     */
     @Test
     public void testSameNumbers() throws Exception {
         for(int i=0;i<arrMultipleNumbers.length;i++)
@@ -36,7 +44,10 @@ public class StandardDeviationTest {
         double expectres = 0;
         Assert.assertTrue(res==expectres);
     }
-
+    /**
+     * Unit test for Function Requirement: R4.
+     * @throws Exception
+     */
     @Test
     public void testNegativeNumbers() throws Exception {
         arrMultipleNumbers[0] = -8;
@@ -49,7 +60,10 @@ public class StandardDeviationTest {
         Assert.assertTrue(Double.valueOf(df.format(res))==expectres);
     }
 
-
+    /**
+     * Unit test for Function Requirement: R5
+     * @throws Exception
+     */
     @Test
     public void testPositiveNumbers() throws Exception {
         arrMultipleNumbers[0] = 8;
@@ -61,7 +75,10 @@ public class StandardDeviationTest {
         double expectres = 1.8547236990991407;
         Assert.assertTrue(Double.valueOf(res)==expectres);
     }
-
+    /**
+     * Unit test for Function Requirement: R6
+     * @throws Exception
+     */
     @Test
     public void testDecimalNumbers() throws Exception {
         arrMultipleNumbers[0] = 8.2;
@@ -73,7 +90,10 @@ public class StandardDeviationTest {
         double expectres = 2.2297981971472;
         Assert.assertTrue(Double.valueOf(df.format(res))==expectres);
     }
-
+    /**
+     * Unit test for Function Requirement: R7
+     * @throws Exception
+     */
     @Test
     public void testSquareRoot() throws Exception {
         double input = 2;
@@ -81,7 +101,10 @@ public class StandardDeviationTest {
         double expectres = 1.4142135623746899;
         Assert.assertTrue(res==expectres);
     }
-
+    /**
+     * Unit test for Function Requirement: R8
+     * @throws Exception
+     */
     @Test
     public void testPower() throws Exception {
         double base = 5;
@@ -90,7 +113,10 @@ public class StandardDeviationTest {
         double expectres = 25;
         Assert.assertTrue(res==expectres);
     }
-
+    /**
+     * Unit test for Function Requirement: R9
+     * @throws Exception
+     */
     @Test
     public void testInputIsNumber() throws Exception {
         String s = "g";
@@ -99,6 +125,15 @@ public class StandardDeviationTest {
         boolean res1 = Eternity.numericInputCheck(s1);
         Assert.assertFalse(res);
         Assert.assertTrue(res1);
+    }
+    /**
+     * Unit test for Function Requirement: R10
+     * @throws Exception
+     */
+    @Test
+    public void testAvailability() throws Exception {
+        //takes around 969ms
+        Eternity e = new Eternity();
     }
 
 }
